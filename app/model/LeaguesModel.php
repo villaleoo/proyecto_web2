5bab82d5df434b45bd64c9844448fcbdc501a523
leopoldo <villaleoo@hotmail.com>
2023-10-10 00:45:55 -0300

    detalles

--- app/model/LeaguesModel.php
+++ app/model/LeaguesModel.php
@@ -3,13 +3,7 @@ require_once './app/controller/LeagueController.php';
 require_once './app/model/Model.php';
 class LeaguesModel extends Model {
 
-    public function getLeagueVinculedTeam($leagueId){
-        $request = $this->db->prepare("SELECT * FROM ligas WHERE id = ?");
-        $request->execute([$leagueId]);
-        $league = $request->fetch(PDO::FETCH_OBJ);
-
-        return $league;
-    }
+ 
 }
 
 ?>
